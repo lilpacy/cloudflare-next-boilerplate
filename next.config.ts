a79518b267b4281f4ccfaf3e6ee9c8845069a6a8
lilpacy <revivedtomorrow@gmail.com>
2025-12-30 06:40:33 +0900

    fix: tarbopack detect wrong root dir

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 // added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
-import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 initOpenNextCloudflareForDev();
 
 const nextConfig: NextConfig = {
@@ -11,9 +11,12 @@ const nextConfig: NextConfig = {
   },
   experimental: {
     serverActions: {
-      bodySizeLimit: '5mb', // for uploading profile image
+      bodySizeLimit: "5mb", // for uploading profile image
     },
-  }
+  },
+  turbopack: {
+    root: __dirname,
+  },
 };
 
 export default nextConfig;
